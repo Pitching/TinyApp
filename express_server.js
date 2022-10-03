21815f1c6e08efc6050c3ab1658150add43e765e
Andrei Chirita <adchir95@gmail.com>
2022-10-03 20:40:53 +0000

    Adding a header file for th tinyapp and passing parameters to the urls_show ejs file

--- express_server.js
+++ express_server.js
@@ -27,7 +27,7 @@ app.get("/hello", (req, res) => {
 });
 
 app.get("/urls/:id", (req, res) => {
-  const templateVars = { id: req.params.id, longURL: urlDatabase.id };
+  const templateVars = { id: req.params.id, longURL: urlDatabase[req.params.id] };
   res.render("urls_show", templateVars);
 });
 
