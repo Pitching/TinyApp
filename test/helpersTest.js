3c5840ccb52f4919b1898a32ce8b9003b000c4a8
Andrei Chirita <adchir95@gmail.com>
2022-10-06 21:07:41 +0000

    updating some chai tests

--- test/helpersTest.js
+++ test/helpersTest.js
@@ -6,7 +6,7 @@ const testUrlDatabase = {
   "ekd72w": {
     longURL: "http://youtube.com",
     userID: "userRandomID"
-  },  
+  },
   "1234fe": {
     longURL: "http://youtube2.com",
     userID: "userRandomID"
@@ -19,26 +19,26 @@ const testUrlDatabase = {
 
 const testUsers = {
   "userRandomID": {
-    id: "userRandomID", 
-    email: "user@example.com", 
+    id: "userRandomID",
+    email: "user@example.com",
     password: "purple-monkey-dinosaur"
   },
   "user2RandomID": {
-    id: "user2RandomID", 
-    email: "user2@example.com", 
+    id: "user2RandomID",
+    email: "user2@example.com",
     password: "dishwasher-funk"
   }
 };
 
-describe('getUserByEmail', function() {
+describe('getUserByEmail', function () {
 
-  it('should return a user with valid email', function() {
+  it('should return a user with valid email', function () {
     const user = lookUpEmail("user@example.com", testUsers);
     const expectedUserID = "userRandomID";
     assert.equal(user, expectedUserID);
   });
 
-  it('should return undefined if there are no users with the email found', function() {
+  it('should return undefined if there are no users with the email found', function () {
     const user = lookUpEmail("unavailable@test.ca", testUsers);
     const expectedUserID = undefined;
     assert.equal(user, expectedUserID);
@@ -46,15 +46,15 @@ describe('getUserByEmail', function() {
 
 });
 
-describe('cookieCheck', function() {
+describe('cookieCheck', function () {
 
-  it('should return true if a user with valid id is found', function() {
+  it('should return true if a user with valid id is found', function () {
     const user = cookieCheck("userRandomID", testUsers);
     const expectedUserID = true;
     assert.equal(user, expectedUserID);
   });
 
-  it('should return false if there are no users with provided id', function() {
+  it('should return false if there are no users with provided id', function () {
     const user = cookieCheck("unavailable@test.ca", testUsers);
     const expectedUserID = false;
     assert.equal(user, expectedUserID);
@@ -62,27 +62,21 @@ describe('cookieCheck', function() {
 
 });
 
-describe('urlsForUser', function() {
+describe('urlsForUser', function () {
 
-  it('should return an object with the url(s) for the user ID', function() {
+  it('should return an object with the short and long url(s) for the user ID', function () {
     const sites = urlsForUser("userRandomID", testUrlDatabase);
-    const expectedUserID = {
-      "ekd72w": {
-        longURL: "http://youtube.com",
-        userID: "userRandomID"
-      },  
-      "1234fe": {
-        longURL: "http://youtube2.com",
-        userID: "userRandomID"
-      }
-    };
-    assert.deepEqual(sites, expectedUserID);
+    const expectedURL = {
+      "ekd72w": "http://youtube.com",
+      "1234fe": "http://youtube2.com"
+    }
+    assert.deepEqual(sites, expectedURL);
   });
 
-  it('should return an empty object with for the user ID if no associated urls found', function() {
+  it('should return an empty object with for the user ID if no associated urls found', function () {
     const sites = urlsForUser("user3RandomID", testUrlDatabase);
-    const expectedUserID = {}
-    assert.deepEqual(sites, expectedUserID);
+    const expectedURL = {}
+    assert.deepEqual(sites, expectedURL);
   });
 
 });
